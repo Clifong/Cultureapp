8a4c744e5d372ca2fc68f4af4b9ded3f5ef318a3
Clifong <59185915+Clifong@users.noreply.github.com>
2024-10-26 11:43:26 +0800

    2. Added more stuff + preparing for databases
    2. Added more stuff + preparing for databases

---
+++ app/src/main/java/com/example/student_hacks/Custom_classes/Database/Database.kt
@@ -0,0 +1,7 @@
+package com.example.student_hacks.Custom_classes.Database
+
+abstract class Database {
+    fun getCredential(id: Int) {}
+
+    fun getFriendList(id: Int) {}
+}
---
+++ app/src/main/java/com/example/student_hacks/Custom_classes/User class/User.kt
@@ -0,0 +1,9 @@
+package com.example.student_hacks.Custom_classes.User
+
+class User(
+    val id: Int,
+    val username: String,
+) {
+
+}
+
