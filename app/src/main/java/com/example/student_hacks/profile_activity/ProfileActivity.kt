af2d90dfe271f947f6c266f840fc930f6b46bbfb
Clifong <59185915+Clifong@users.noreply.github.com>
2024-11-10 13:36:42 +0800

    Tweak
    Tweak

--- app/src/main/java/com/example/student_hacks/profile_activity/ProfileActivity.kt
+++ app/src/main/java/com/example/student_hacks/profile_activity/ProfileActivity.kt
@@ -30,14 +30,19 @@ class ProfileActivity : AppCompatActivity() {
         val savedUsername = sharedPreferences.getString("username", Database.user.username)
         val savedAge = sharedPreferences.getInt("age", Database.user.age)
         val savedCountry = sharedPreferences.getString("country", Database.user.country)
-
+        val savedAboutMe = sharedPreferences.getString("aboutMe", Database.user.aboutMe)
 
         usernameEditText = findViewById(R.id.profileUsernameEditText)
         usernameEditText.setText(savedUsername)
 
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         ageSpinner = findViewById(R.id.ageSpinner)
         countrySpinner = findViewById(R.id.countrySpinner)
 
+        aboutMeEditText = findViewById(R.id.profileAboutMeEditText)
+        aboutMeEditText.setText(savedAboutMe)
 
         val allCountries = Locale.getAvailableLocales().map { it.displayCountry }.distinct()
         val allAges = (18..50).toList()
@@ -54,19 +59,23 @@ class ProfileActivity : AppCompatActivity() {
             val username = usernameEditText.text.toString()
             val age = ageSpinner.selectedItem as Int
             val country = countrySpinner.selectedItem as String
+            val aboutMe = aboutMeEditText.text.toString()
 
-            Database.updateProfile(username, age, country, onSuccess = {
+            Database.updateProfile(username, age, country, aboutMe, onSuccess = {
 
                 val editor = sharedPreferences.edit()
                 editor.putString("username", username)
                 editor.putInt("age", age)
                 editor.putString("country", country)
+                editor.putString("aboutMe", aboutMe)
                 editor.apply()
 
                 Toast.makeText(this, "Successfully updated profile!", Toast.LENGTH_SHORT).show()
             }, onFailure = { e ->
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-            })
+            },
+
+            )
         }
     }
 }
